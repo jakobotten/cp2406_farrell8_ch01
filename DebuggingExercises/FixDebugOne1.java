4a8235bd3656856a21a37b57f194b7112ce4f3cc
jakobotten <jakob.otten@my.jcu.edu.au>
2017-08-01 08:49:19 +1000

    Remove syntax errors

--- DebuggingExercises/FixDebugOne1.java
+++ DebuggingExercises/FixDebugOne1.java
@@ -1,7 +1,9 @@
-public class DebugOne1
+public class FixDebugOne1 {
 
    /* This program displays a greeting */
    public static void main(String[] args)
    {
-      Systemoutprintl("Hello).
+      System.out.println("Hello");
    }
+
+}
