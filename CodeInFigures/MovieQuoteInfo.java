65c93a53d84e901c36052286a70c9a391c3e44d4
jakobotten <jakob.otten@my.jcu.edu.au>
2017-08-01 09:52:48 +1000

    Add MovieQuoteInfo.java and TableAndChairs.java

---
+++ CodeInFigures/MovieQuoteInfo.java
@@ -0,0 +1,7 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("I'm gonna make him an offer he can't refuse.");
+        System.out.println("                            -Don Vito Corleone (Marlon Brando)");
+        System.out.println("                                    The Godfather (1972)");
+    }
+}
