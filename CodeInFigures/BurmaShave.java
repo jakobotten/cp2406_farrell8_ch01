dd392771007c91d0cbe27b3f3efde461f49afc29
jakobotten <jakob.otten@my.jcu.edu.au>
2017-08-02 14:32:16 +1000

    Add last example

---
+++ CodeInFigures/BurmaShave.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class BurmaShave {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Feel your face");
+        JOptionPane.showMessageDialog(null,"As you ride by");
+        JOptionPane.showMessageDialog(null,"Now don't");
+        JOptionPane.showMessageDialog(null,"You think");
+        JOptionPane.showMessageDialog(null,"It's time to try");
+        JOptionPane.showMessageDialog(null,"Burma-Shave");
+    }
+}
